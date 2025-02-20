7ed066d0199a9a8cde307f91f96ef60bbd0bcdd7
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-02-20 18:06:55 -0300

    feat: refactor PokemonInfo to use createResource and improve loading fallback

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,31 +2,15 @@
 // http://localhost:3000/isolated/final/01.extra-2.js
 
 import * as React from 'react'
-import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
+import {
+  fetchPokemon,
+  PokemonDataView,
+  PokemonErrorBoundary,
+  PokemonInfoFallback,
+} from '../pokemon'
+import {createResource} from 'utils'
 
 let pokemonResource = createResource(fetchPokemon('pikachu'))
-
-function createResource(promise) {
-  let status = 'pending'
-  let result = promise.then(
-    resolved => {
-      status = 'success'
-      result = resolved
-    },
-    rejected => {
-      status = 'error'
-      result = rejected
-    },
-  )
-  return {
-    read() {
-      if (status === 'pending') throw result
-      if (status === 'error') throw result
-      if (status === 'success') return result
-      throw new Error('This should be impossible')
-    },
-  }
-}
 
 function PokemonInfo() {
   const pokemon = pokemonResource.read()
@@ -45,7 +29,7 @@ function App() {
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         <PokemonErrorBoundary>
-          <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <React.Suspense fallback={<PokemonInfoFallback />}>
             <PokemonInfo />
           </React.Suspense>
         </PokemonErrorBoundary>

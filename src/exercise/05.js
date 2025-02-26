8728e57cbf8ed1361b6f1d3f688a9d8a9ee347b1
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-02-26 19:46:30 -0300

    refactor: update image loading URL and remove unused PokemonDataView component

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,5 +1,5 @@
 // Suspense Image
-// http://localhost:3000/isolated/final/05.extra-1.js
+// http://localhost:3000/isolated/final/05.extra-2.js
 
 import * as React from 'react'
 import {
@@ -7,10 +7,13 @@ import {
   getImageUrlForPokemon,
   PokemonInfoFallback,
   PokemonForm,
-  PokemonDataView,
   PokemonErrorBoundary,
 } from '../pokemon'
 import {createResource} from '../utils'
+
+const PokemonInfo = React.lazy(() =>
+  import('../lazy/pokemon-info-render-as-you-fetch'),
+)
 
 function preloadImage(src) {
   return new Promise(resolve => {
@@ -18,18 +21,6 @@ function preloadImage(src) {
     img.src = src
     img.onload = () => resolve(src)
   })
-}
-
-function PokemonInfo({pokemonResource}) {
-  const pokemon = pokemonResource.data.read()
-  return (
-    <div>
-      <div className="pokemon-info__img-wrapper">
-        <img src={pokemonResource.image.read()} alt={pokemon.name} />
-      </div>
-      <PokemonDataView pokemon={pokemon} />
-    </div>
-  )
 }
 
 const SUSPENSE_CONFIG = {
